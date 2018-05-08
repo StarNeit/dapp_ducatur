9b5c4c13c737aa63d38eb781ec38cb499424ea56
StarNeit <koneit@outlook.com>
2018-05-08 22:53:11 +0200

    update infura key with ropsten network

--- src/services/sample.js
+++ src/services/sample.js
@@ -4,7 +4,7 @@ var DucaturToken = require('../../build/contracts/DucaturToken.json');
 var HDWalletProvider = require("truffle-hdwallet-provider");
 var Transactions = require('../models/Transactions');
 
-const hdWalletProvider = new HDWalletProvider(process.env.MNE, "https://mainnet.infura.io/");
+const hdWalletProvider = new HDWalletProvider(process.env.MNE, "https://ropsten.infura.io/VaxMZqBPDeLCJNBAsNN1");
 const ownerAddress = hdWalletProvider.address;
 const web3 = new Web3(hdWalletProvider);
 const token = contract(DucaturToken);
